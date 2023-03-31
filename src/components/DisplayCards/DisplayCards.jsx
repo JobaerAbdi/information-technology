a5d692a8ce135575dbbb22e87653621bee2209ac
JobaerAbdi <jobaerrahman757@gmail.com>
2023-03-31 10:26:03 +0600

    Author name and image set copmleted

--- src/components/DisplayCards/DisplayCards.jsx
+++ src/components/DisplayCards/DisplayCards.jsx
@@ -7,16 +7,24 @@ const DisplayCards = ({blog}) => {
         <div>
             <div className="card w-full h-full bg-base-100 shadow-xl mt-8">
             <figure><img src={coverImage} /></figure>
-            <div>
-                <img className="w-14 h-14 rounded-full" src={authorImage} />
-            </div>
+
             <div className="card-body">
-                <h2 className="card-title">{authorName}</h2>
-                <h2 className="card-title">{readTime}</h2>
-                <h2 className="card-title">{publishDate}</h2>
-                <p>{blogTitle}</p>
-                <div className="card-actions justify-end">
-                <button className="btn btn-primary">Buy Now</button>
+                <div className='flex justify-between items-center'>
+                    <div className='flex items-center'>
+                        <div className=''>
+                           <img className="w-14 h-14 rounded-full" src={authorImage} />
+                        </div>
+                        <div className='ml-4'>
+                            <h2 className="font-bold text-1xl">{authorName}</h2>
+                            <h2>{publishDate}</h2>
+                        </div>
+                    </div>
+                    <div>
+                        <h2>0{readTime} Minute Red</h2>
+                    </div>
+                </div>
+                <div className="mt-4">
+                    <h1 className='font-bold text-2xl'>{blogTitle}</h1>
                 </div>
             </div>
             </div>
